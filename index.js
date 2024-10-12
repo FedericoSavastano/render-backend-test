a7f77b5b04cd95edab42ec2de54446bde92a3c0f
FedericoSavastano <f.savastano@hotmail.com>
2024-10-12 20:52:28 -0300

    fin de anteultimos ejercicios

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ require('dotenv').config()
 
 const app = express();
 
+
 app.use(express.json());
- 
 app.use(express.static('dist'))
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body :host'))
 app.use(cors())
 
@@ -76,10 +77,19 @@ app.get('/api/persons', (request, response) => {
 
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+        if (person) {
+            response.json(person)
+          } else {
+            response.status(404).end()
+          }
     })
+    // .catch(error => {
+    //     console.log(error)
+    //     response.status(400).send({ error: 'malformatted id' })
+    //   })
+      .catch(error => next(error))
   })
 
 
@@ -101,18 +111,40 @@ app.get('/info', (request, response) => {
 // });
 
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    Person.deleteOne({
-        _id: id
-    }).then(deletedPerson => {
-        response.json(deletedPerson)
-      })
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = request.params.id;
+//     Person.deleteOne({
+//         _id: id
+//     }).then(deletedPerson => {
+//         response.json(deletedPerson)
+//       })
+// })
+    
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
     
     
-    
-    
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+   
+
+  const person = {
+    name ,
+    number ,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true , runValidators: true, context: 'query'})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
     
      
  
@@ -153,8 +185,8 @@ app.delete('/api/persons/:id', (request, response) => {
 
 
 
-app.post('/api/persons/', (request, response) => {
-    const body = request.body
+app.post('/api/persons/', (request, response, next) => {
+    const body = request.body;
   
     if (!body.name || !body.number) {
                 return response.status(400).json({
@@ -178,11 +210,32 @@ app.post('/api/persons/', (request, response) => {
     newPerson.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
   })
 
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+  // handler of requests with unknown endpoint
+app.use(unknownEndpoint)
 
 
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,72 +1,82 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+// const mongoose = require('mongoose')
+// require('dotenv').config()
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const password = process.argv[2]
+// const password = process.argv[2]
 
-const name = process.argv[3]
+// const name = process.argv[3]
 
-const number = process.argv[4]
+// const number = process.argv[4]
 
-// const url =
-//   `mongodb+srv://federicosavastano1986:${password}@cluster0.gczjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// // const url =
+// //   `mongodb+srv://federicosavastano1986:${password}@cluster0.gczjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const url = process.env.MONGODB_URI;
-// console.log("url ", url)
+// const url = process.env.MONGODB_URI;
+// // console.log("url ", url)
 
-mongoose.set('strictQuery',false)
+// mongoose.set('strictQuery',false)
 
-mongoose.connect(url)
+// mongoose.connect(url)
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+// const personSchema = new mongoose.Schema({
+   
+//   name: {
+//     type: String,
+//     minLength: 3,
+//     required: true
+//   },
+   
+//   number: {
+//     type: String,
+//     minLength: 8,
+//     required: true
+//   },
+// })
 
 
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+// personSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
 
-const Person = mongoose.model('Person', personSchema)
+// const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-    name,
-    number,
-})
+// const person = new Person({
+//     name,
+//     number,
+// })
 
 
  
 
 
-if(!name || !password) {
-    Person.find({}).then(result => {
-        console.log("Phonebook:")
-        result.forEach(person => {
-          console.log(person)
-        })
-        mongoose.connection.close()
-      })
+// if(!name || !password) {
+//     Person.find({}).then(result => {
+//         console.log("Phonebook:")
+//         result.forEach(person => {
+//           console.log(person)
+//         })
+//         mongoose.connection.close()
+//       })
 
 
       
-} else {
-    person.save().then(result => {
-        console.log( `added ${name} number ${number} to phonebook` )    
-        mongoose.connection.close()
-      })
+// } else {
+//     person.save().then(result => {
+//         console.log( `added ${name} number ${number} to phonebook` )    
+//         mongoose.connection.close()
+//       })
 
 
       
-}
+// }
 
 
 
